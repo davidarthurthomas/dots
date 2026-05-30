4f3ee8b7d68d486ab1921beeb3a6bee212912239
David Thomas <dthomas2002@gmail.com>
2026-05-29 21:01:57 -0400

    Add OrbStack shell integration to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,7 @@ fish_add_path /Applications/Obsidian.app/Contents/MacOS
 mise activate fish | source
 fzf --fish | source
 zoxide init fish --cmd cd | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
